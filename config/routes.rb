327c4fd21d0d55537148928e3aa3e4741cac23b6
rarvear <rarvear@gmail.com>
2018-08-17 22:18:43 -0300

    add admin

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,12 @@ Rails.application.routes.draw do
   get 'inscriptions/new'
   post 'inscriptions', to: 'inscriptions#create', as: 'inscription_registration'
   devise_for :users
-  resources :courses
+  namespace :admins do
+    resources :users
+    resources :courses
+  end 
+
   root 'courses#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
